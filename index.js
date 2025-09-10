1be45d9f6d56fbccdd259c3bae7d28a9562bb38a
Herbert-C-byte <epsylon344@gmail.com>
2025-09-11 00:00:20 +0100

    tryy

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
-.then(data => data.json())
-.then(coin => {
-  console.log(`Current Bitcoin price: $${coin.market_data.current_price.usd} `);
+// POST a new post
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'POST',
+  headers: {'COntent-Type: '}
+}
+.then(res => {
+
+})
+
+// GET a list of posts
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+.then(res => {
+  console.log('Status: ', res.status);  // Should be 200
+})
+.then(data => {
+  console.log('GET result: ', data);
 })
 .catch(err => {
-  console.error("Something went really wrong: ", err);
-}) 
-
-
-// fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
-// .then(data => data.json())
-// .then(coin => {
-//   console.log(`Here's today's price of bitcoin in the market: `, coin.market_data.current_price.usd, `USD`);
-// })
-// .catch(error => {
-//   console.log("Something went really wrong: ", error);
-// }) 
+  console.log('Something went really wrong bro: ', err);
+})
